9b0666e7d0bc9dfe9ffc2fd6999a13453594e3b5
RicardoCalderon <112725005+Pabloricalderon@users.noreply.github.com>
2025-05-12 21:57:46 -0600

    Reboot

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,19 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+    public float speed = 200;
+    public float rotationspeed = 200;
+    private float horizontalinput;
+    private float verticalinput;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalinput = Input.GetAxis("Horizontal");
+        verticalinput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalinput);
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationspeed * horizontalinput);
     }
 }
